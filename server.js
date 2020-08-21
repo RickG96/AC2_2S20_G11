2ce66256c712039b6a5da59ded39af719eba15c7
Ricardo <rick96_@hotmail.com>
2020-08-20 23:40:34 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+var objeto = {
+    load: true,
+    weight: 5,
+    waterLevel: 3
+}
+
+app.get('/', (req, res) => {
+    res.json(objeto);
+});
+
+
+app.listen(3000, (err) => {
+    if (err) console.log('Ocurrio un error'), process.exit(1);
+
+    console.log('Escuchando en el puerto 3000');
+})
